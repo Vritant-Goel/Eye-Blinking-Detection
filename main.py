5b26535e8536de767491c728a5e439cf2a797c88
Kritika Mittal <115084406+kri-ti-ka@users.noreply.github.com>
2023-06-02 22:40:33 +0530

    Add files via upload
    added comments, added gray frame for fun

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ while True:
     ret, frame = cap.read()
     if not ret:
         continue
-
+#below code converts all the colors to gray as to make the roi(region of interest) pop. so we can later on work upon roi
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow("Frame", frame)
+    cv2.imshow("gray frame", gray_frame)
 
 
 
