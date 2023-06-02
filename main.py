74365dbfca55f592fac05062d3e4a01ede544725
Kritika Mittal <115084406+kri-ti-ka@users.noreply.github.com>
2023-06-02 22:25:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,3 +3,25 @@ import numpy as np
 
 # init camera
 cap = cv2.VideoCapture(0)
+
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        continue
+
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow("Frame", frame)
+
+
+
+
+
+    key_pressed = cv2.waitKey(1) & 0xFF
+    if key_pressed == ord('q'):
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
